7c7579ecf173769402f3919f51320d583a7fb4a1
Mg Kyaing <mr.kyaing7@gmail.com>
2024-01-27 22:20:34 +0630

    web site practices

---
+++ EWADB3/WorkOut/Controllers/ConvertorController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WorkOut.Controllers
+{
+    public class ConvertorController : Controller
+    {
+        public IActionResult CurrencyConvertorV1()
+        {
+            return View();
+        }
+        //localhost:3000/Convertor/CurrencyConvertorV1
+        [HttpPost]
+        public IActionResult CurrencyConvertorV1(string selectedCurrency,decimal amout)
+        {
+            decimal convertedAmount = 0;
+            switch (selectedCurrency)
+            {
+                case "USD":convertedAmount = amout * 3000;break;
+                case "SDG":convertedAmount = amout * 1200;break;
+                case "TBH":convertedAmount = amout * 100;break;
+            }
+            ViewBag.Amount = convertedAmount;
+            return View();
+        }
+
+
+        public IActionResult CurrencyConvertorV2()
+        {
+            return View();
+        }
+        //localhost:3000/Convertor/CurrencyConvertorV1
+        [HttpPost]
+        public IActionResult CurrencyConvertorV2(string selectedCurrency, decimal amout)
+        {
+            List<string> errors = new();
+            if (string.IsNullOrEmpty(selectedCurrency))
+            {
+                errors.Add("please select a currency");
+            }
+            if (amout <=0)
+            {
+                errors.Add("invalid amount");
+            }
+            if (errors.Count > 0)
+            {
+                ViewBag.Errors = errors;
+                return View();
+            }
+            decimal convertedAmount = 0;
+            switch (selectedCurrency)
+            {
+                case "USD": convertedAmount = amout * 3000; break;
+                case "SDG": convertedAmount = amout * 1200; break;
+                case "TBH": convertedAmount = amout * 100; break;
+            }
+            ViewBag.selectedCurrency = selectedCurrency;
+            ViewBag.inputAmount = amout;
+            ViewBag.Amount = convertedAmount;
+            return View();
+        }
+    }
+}
---
+++ EWADB3/WorkOut/Program.cs
@@ -0,0 +1,6 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+//app.MapDefaultControllerRoute();
+app.MapControllerRoute(name: "default", pattern: "{controller=home}/{action=index}/{id?}");
+app.Run();
