70709df91ec325e1b49df9b6f2723ed427b4d5de
mrkyaing <akog02@gmail.com>
2024-02-24 22:48:55 +0630

    Identity and AdminLTE

--- EWADB3/CloudHRMS/obj/Debug/net6.0/CloudHRMS.AssemblyInfo.cs
+++ EWADB3/CloudHRMS/obj/Debug/net6.0/CloudHRMS.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudHRMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783e1f7043eea37b8d269f72af9ea77626bfd50c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudHRMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudHRMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
